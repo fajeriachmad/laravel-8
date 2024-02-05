dd8f8f217db3b67df55a84e46225d3c3e54d9f61
Ahmad Fajri Rahman <ahmad.fajri@ai.astra.co.id>
2024-02-05 17:42:34 +0700

    Fix: n-plus-one problem

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,9 +12,14 @@ class PostController extends Controller
     public function index()
     {
         return view('posts', [
-            "title" => "Posts",
+            'title' => 'Show all posts',
             // use ModelName::latest()->get() to sort the data in descending order based on the created_at
-            "posts" => Post::latest()->get()
+            // 'posts' => Post::latest()->get()
+
+            // use ModelName::with(['related_field', 'related_field', ...]) to avoid lazy loading
+            // which can minimize the total amount of queries
+            // https://laravel.com/docs/8.x/eloquent-relationships#eager-loading
+            'posts' => Post::with(['author', 'category'])->latest()->get()
             // "posts" => Post::all()
         ]);
     }
@@ -20,8 +27,29 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('post', [
-            "title" => "Single Post",
-            "post" => $post
+            'title' => 'Single Post',
+            'post' => $post
+        ]);
+    }
+
+    public function showByCategory(Category $category)
+    {
+        return view('posts', [
+            'title' => "Post by Category : $category->name",
+            // 'posts' => $category->posts
+
+            // use $model_name->attribute(if used)->load(['relation_field', 'relation_field', ...]) to perform lazy load
+            // which also can minimize the total amount of queries
+            // https://laravel.com/docs/8.x/eloquent-relationships#lazy-eager-loading 
+            'posts' => $category->posts->load(['category', 'author'])
+        ]);
+    }
+
+    public function showByAuthor(User $author)
+    {
+        return view('posts', [
+            'title' => "Post by Author : $author->name",
+            'posts' => $author->posts->load(['category', 'author'])
         ]);
     }
 }
--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,7 +3,7 @@ $('document').ready( function () {
         $(".nav-item.active").removeClass("active");  
         $(this).addClass("active");
     }).filter( function () {
-        console.log(window.location.href.indexOf($(this).attr('data-href').trim()) > -1);
+        // console.log(window.location.href.indexOf($(this).attr('data-href').trim()) > -1);
         return window.location.href.indexOf($(this).attr('data-href').trim()) > -1;
     }).click();
 });
